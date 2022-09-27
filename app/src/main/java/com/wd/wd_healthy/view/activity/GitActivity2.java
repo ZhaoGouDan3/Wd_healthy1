7f5d25829435f80873d5169c7087e6c8ba434ec6
target_username <1310031723@qq.com>
2022-09-27 16:31:51 +0800

    1111

---
+++ app/src/main/java/com/wd/wd_healthy/view/activity/GitActivity2.java
@@ -0,0 +1,11 @@
+package com.wd.wd_healthy.view.activity;
+
+/**
+ * <p>Wd_healthy</p>
+ * <p>包名:com.wd.wd_healthy.view.activity</p>
+ *
+ * @author 赵某某
+ * @date 2022/9/27
+ */
+public class GitActivity2 {
+}
