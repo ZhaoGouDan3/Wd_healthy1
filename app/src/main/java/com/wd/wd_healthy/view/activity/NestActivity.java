1685f25bc4123f3c393ebebe7a75ad5f57a672c2
target_username <1310031723@qq.com>
2022-10-04 16:39:19 +0800

    1111

--- app/src/main/java/com/wd/wd_healthy/view/activity/NestActivity.java
+++ app/src/main/java/com/wd/wd_healthy/view/activity/NestActivity.java
@@ -1,7 +1,12 @@
 package com.wd.wd_healthy.view.activity;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +15,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.wd.wd_healthy.R;
+import com.wd.wd_healthy.model.util.SlideRecyclerView;
 import com.zhy.adapter.recyclerview.CommonAdapter;
 import com.zhy.adapter.recyclerview.base.ViewHolder;
 
@@ -18,7 +24,7 @@ import java.util.List;
 
 public class NestActivity extends AppCompatActivity {
 
-    private RecyclerView rv;
+    private SlideRecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +44,37 @@ public class NestActivity extends AppCompatActivity {
             }
         });
         rv.setLayoutManager(new LinearLayoutManager(this));
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        Log.e("TAG", "densityDpi: " + displayMetrics.densityDpi);
+        Log.e("TAG", "density: " + displayMetrics.density);
+        Log.e("TAG", "widthPixels: " + displayMetrics.widthPixels);
+        Log.e("TAG", "heightPixels: " + displayMetrics.heightPixels);
+
+    }
+    private static final int INVALID_POSITION = -1; // 触摸到的点不在子View范围内
+    private Rect mTouchFrame;   // 子View所在的矩形范围
+    public int pointToPosition(int x, int y) {
+        int firstPosition = ((LinearLayoutManager) rv.getLayoutManager()).findFirstVisibleItemPosition();
+        Rect frame = mTouchFrame;
+        if (frame == null) {
+            mTouchFrame = new Rect();
+            frame = mTouchFrame;
+        }
+
+        final int count = rv.getChildCount();
+        for (int i = count - 1; i >= 0; i--) {
+            final View child = rv.getChildAt(i);
+            if (child.getVisibility() == View.VISIBLE) {
+                child.getHitRect(frame);
+                if (frame.contains(x, y)) {
+                    return firstPosition + i;
+                }
+            }
+        }
+        return INVALID_POSITION;
     }
 
     private void initView() {
-        rv = (RecyclerView) findViewById(R.id.rv);
+        rv = (SlideRecyclerView) findViewById(R.id.rv);
     }
 }
